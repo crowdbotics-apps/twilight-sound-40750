b20bc9575ab7b4d0ab453238c04b63e1abee6cde
Crowdbotics <team@crowdbotics.com>
2023-05-16 22:40:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-sound-40750",
+  url: "https://twilight-sound-40750.botics.co",
+  api: "https://twilight-sound-40750.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
